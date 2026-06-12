2f854101d152da7af0458f26cb39cb9c65e8d5bd
Soseh <sosehv@gmail.com>
2026-06-12 16:49:00 -0400

    Adds logging and env configuration

--- conftest.py
+++ conftest.py
@@ -2,14 +2,16 @@ import pytest
 from core.client.api_client import APIClient
 from fixtures.user_fixtures import *
 from core.config.config import BASE_URL_API, API_KEY
+import logging
+
+logging.getLogger("faker").setLevel(logging.WARNING)
+
 
 @pytest.fixture(scope="session")
 def headers():
-  return {
-    "x-api-key": API_KEY,
-    "Content-Type": "application/json"
-  }
+    return {"x-api-key": API_KEY, "Content-Type": "application/json"}
+
 
 @pytest.fixture
 def api_client(headers):
-  return APIClient(BASE_URL_API, headers)
+    return APIClient(BASE_URL_API, headers)
